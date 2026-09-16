265b3d7749b07c700878c46d5d126fc9dbd2c918
Abdelwahab-Mh <awwad7959@gmail.com>
2026-09-17 00:14:40 +0300

    Add files via upload

---
+++ Interactive Math Quiz Game/Project 2/Project 2.cpp
@@ -0,0 +1,189 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+enum enLevel { Easy = 1, Med = 2, Hard = 3 , Mix=4 };
+enum enType { Add = 1, Sub = 2, Mul = 3 , Div = 4 , Mix2 = 5 };
+int RandomNum(int From, int To)
+{
+	int randnum = rand() % (To - From + 1) + From;
+	return randnum;
+}
+
+int ReadNumQues()
+{
+	int N;
+	cout << "How Many Question you want to ansower ?  ";
+	cin >> N;
+	return N;
+}
+
+int QuesLevel(int NumLevel)
+{
+	int Num;
+	if (NumLevel == enLevel::Mix)
+	{
+		NumLevel = RandomNum(1, 3);
+	}
+	switch (NumLevel)
+	{
+		case enLevel::Easy:
+			Num = RandomNum(1, 20);
+			break;
+		case enLevel::Med:
+			Num = RandomNum(1, 50);
+			break;
+		case enLevel::Hard:
+			Num = RandomNum(1, 100);
+			break;
+		default :
+			break;
+	}
+	return Num;
+}
+
+void checkResult(int result,int & Counter)
+{
+	int N;
+	cin >> N;
+	if (N == result)
+	{
+		cout << "Right Answer :-)" << endl;
+		system("color 2F");
+		Counter++;
+	}
+	else
+	{
+		cout << "Wrong Answer :-(" << endl;
+		cout << "The Right Answer is: "<< result << endl;
+		system("color 4F");
+
+	}
+}
+
+int QuesType(int NumType,int NumLevel)
+{
+	int Num1 = QuesLevel(NumLevel);
+	int Num2 = QuesLevel(NumLevel);
+	int result;
+	if (NumType == enType::Mix2)
+	{
+		NumType = RandomNum(1, 4);
+	}
+	switch (NumType)
+	{
+	case enType::Add:
+		cout << Num1 << "\n" << Num2<< "+" << endl;
+		cout << "---------" << endl;
+		result=Num1 + Num2;
+		break;
+	case enType::Sub:
+		cout << Num1 << "\n" << Num2 << "-" << endl;
+		cout << "---------" << endl;
+		result=Num1 - Num2;
+		break;
+	case enType::Mul:
+		cout << Num1 << "\n" << Num2 << "*" << endl;
+		cout << "---------" << endl;
+		result=Num1 * Num2;
+		break;
+	case enType::Div:
+		cout << Num1 << "\n" << Num2 << "/" << endl;
+		cout << "---------" << endl;
+		result=Num1 / Num2;
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+string SelectQuesLevel(int NumLevel)
+{
+	switch (NumLevel)
+	{
+	case 1:
+		return "Easy";
+	case 2:
+		return "Med";
+	case 3:
+		return "Hard";
+	case 4:
+		return "Mix";
+	}
+}
+
+string SelectQuesType(int NumType)
+{
+	switch (NumType)
+	{
+	case 1:
+		return "Add";
+	case 2:
+		return "Sub";
+	case 3:
+		return "Mul";
+	case 4:
+		return "Div";
+	case 5:
+		return "Mix";
+	}
+}
+
+void StartGame(int N)
+{
+	int NumQusLevel, NumQusType,Counter=0;
+	char PlayAgan = 'Y';
+	while(PlayAgan=='Y' || PlayAgan=='y')
+	{
+		cout << "Enter Questions Level [1] Easy, [2] Med, [3] Hard, [4] Mix ?  ";
+		cin >> NumQusLevel;
+		cout << "Enter Questions Type [1] Add, [2] Sub, [3] Mul, [4] Div, [5] Mix ?  ";
+		cin >> NumQusType;
+		while (!((NumQusLevel > 0 && NumQusLevel <= 4) && (NumQusType > 0 && NumQusType <= 5)))
+		{
+			cout << "Invalid Number !!" << endl;
+			cout << "Enter Questions Level [1] Easy, [2] Med, [3] Hard, [4] Mix ?  ";
+			cin >> NumQusLevel;
+			cout << "Enter Questions Type [1] Add, [2] Sub, [3] Mul, [4] Div, [5] Mix ?  ";
+			cin >> NumQusType;
+		}
+		for (int i = 1; i <= N; i++)
+		{
+			cout << "Question [" << i << "/" << N << "]" << endl;
+			checkResult(QuesType(NumQusType, NumQusLevel), Counter);
+		}
+		cout << "--------------------------" << endl;
+		if (Counter>(N-Counter))
+		{
+			cout << "Final Resultls is Pass :-)" << endl;
+			system("color 2F");
+		}
+		else
+		{
+			cout << "Final Resultls is Faild :-(" << endl;
+			system("color 4F");
+		}
+		cout << "--------------------------" << endl;
+
+		cout << "Number Of Questions: "<<N << endl;
+		cout << "Questions Level    : "<< SelectQuesLevel(NumQusLevel) << endl;
+		cout << "Questions Type     : " << SelectQuesType(NumQusType) << endl;
+		cout << "Number Right Questions  : "<<Counter << endl;
+		cout << "Number Wrong Questions  : " <<N-Counter<< endl;
+		cout << "--------------------------\n" << endl;
+		cout << "Do you want play agan ? Y/N ?";
+		cin >> PlayAgan;
+		if (PlayAgan == 'Y' || PlayAgan == 'y')
+		{
+			system("cls");
+			Counter = 0;
+		}
+			
+
+	}
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	StartGame(ReadNumQues());
+}
---
+++ Rock Paper Scissors Game/Project 1/Project 1.cpp
@@ -0,0 +1,156 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+enum enChoose { Stone = 1, Paper = 2 ,Scissors = 3 };
+int PrintRandomNum(int From, int To)
+{
+	int randnum = rand() % (To - From + 1) + From;
+	return randnum;
+}
+void ResultComputer(int ChooseOfComputer)
+{
+	if (ChooseOfComputer == enChoose::Stone)
+	{
+		cout << "Computer Choose: Stone" << endl;
+	}
+	else if (ChooseOfComputer == enChoose::Paper)
+	{
+		cout << "Computer Choose: Paper" << endl;
+	}
+	else if (ChooseOfComputer == enChoose::Scissors)
+	{
+		cout << "Computer Choose: Scissors" << endl;
+	}
+}
+void ResultPlayer(int ChooseOfPlayer)
+{
+	if (ChooseOfPlayer ==enChoose::Stone)
+	{
+		cout << "Player Choose: Stone" << endl;
+	}
+	else if (ChooseOfPlayer == enChoose::Paper)
+	{
+		cout << "Player Choose: Paper" << endl;
+	}
+	else if (ChooseOfPlayer == enChoose::Scissors)
+	{
+		cout << "Player Choose: Scissors" << endl;
+	}
+}
+
+int ReadRoundNum(string S)
+{
+	int Num;
+	cout << S << endl;
+	cin >> Num;
+	return Num;
+}
+
+void CheckWin(int ChooseOfPlayer,int ChooseOfComputer,int &CountPlayer,int &CountComputer,int &CountDrow)
+{
+	if (ChooseOfPlayer == ChooseOfComputer)
+	{
+		cout << "Round Winner: [No Winner]" << endl;
+		CountDrow++;
+		system("color 6");
+	}
+	else if (ChooseOfPlayer==enChoose::Paper && ChooseOfComputer==enChoose::Stone)
+	{
+		cout << "Round Winner: [Player Winner]" << endl;
+		CountPlayer++;
+		system("color 2");
+	}
+	else if (ChooseOfPlayer == enChoose::Paper && ChooseOfComputer == enChoose::Scissors)
+	{
+		cout << "Round Winner: [Computer Winner]" << endl;
+		CountComputer++;
+		system("color 4");
+		cout << "\a";
+	}
+	else if (ChooseOfPlayer == enChoose::Stone && ChooseOfComputer == enChoose::Scissors)
+	{
+		cout << "Round Winner: [Player Winner]" << endl;
+		CountPlayer++;
+		system("color 2");
+	}
+	else if (ChooseOfPlayer == enChoose::Stone && ChooseOfComputer == enChoose::Paper)
+	{
+		cout << "Round Winner: [Computer Winner]" << endl;
+		CountComputer++;
+		system("color 4");
+		cout << "\a";
+	}
+	else if (ChooseOfPlayer == enChoose::Scissors && ChooseOfComputer == enChoose::Paper)
+	{
+		cout << "Round Winner: [Player Winner]" << endl;
+		CountPlayer++;
+		system("color 2");
+	}
+	else if (ChooseOfPlayer == enChoose::Scissors && ChooseOfComputer == enChoose::Stone)
+	{
+		cout << "Round Winner: [Computer Winner]" << endl;
+		CountComputer++;
+		system("color 4");
+		cout << "\a";
+	}
+}
+
+void Game(int NumOfRound)
+{
+	int ChoosePlyar , ChooseComputer,CountPlayer=0,CountComputer=0, CountDrow=0;
+	for (int i=1;i<=NumOfRound;i++)
+	{
+		cout << "Round [" << i << "]" << "begin"<<endl;
+		cout << "Your Choos: [1]:Stone [2]:Paper [3]:Scissors ?";
+		cin >> ChoosePlyar;
+		while (ChoosePlyar < 1 || ChoosePlyar > 3)
+		{
+			cout << "Invalid choice, enter 1-3: ";
+			cin >> ChoosePlyar;
+		}
+
+		cout << "_____________Round [" << i << "]" << endl;
+		ResultPlayer(ChoosePlyar);
+		ChooseComputer = PrintRandomNum(1, 3);
+		ResultComputer(ChooseComputer);
+		CheckWin(ChoosePlyar, ChooseComputer, CountPlayer, CountComputer, CountDrow);
+		cout << "______________________" << endl;
+    }
+	cout << "_________[Game Results]__________" << endl;
+	cout << "Game Round            :"<< NumOfRound << endl;
+	cout << "Player Won            :"<< CountPlayer << endl;
+	cout << "Computer Won          :"<< CountComputer << endl;
+	cout << "Drow Times            :"<< CountDrow << endl;
+	if (CountPlayer> CountComputer)
+	{
+		cout << "Game Winner: [Player Winner]" << endl;
+		system("color 2");
+	}
+	else if (CountPlayer < CountComputer)
+	{
+		cout << "Game Winner: [Computer Winner]" << endl;
+		system("color 4");
+		cout << "\a";
+	}else
+	{ 
+		cout << "Round Winner: [No Winner]" << endl;
+		system("color 6");
+	}
+	cout << "_________________________________" << endl;
+	cout << "Do You want to play agan ? Y/N ?";
+	char Cont;
+	cin >> Cont;
+	if (Cont == 'Y' || Cont == 'y')
+	{
+		system("color 0");
+		system("cls");
+		Game(ReadRoundNum("Please Enter Number Of Round: "));
+	}
+	else if (Cont == 'N' || Cont == 'n')
+	{}
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+	Game(ReadRoundNum("Please Enter Number Of Round: "));
+}
